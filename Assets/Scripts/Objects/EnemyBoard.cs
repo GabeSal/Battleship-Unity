736d0af0ff3b4c3c8e6333b9e1cfc7074e6df632
GabeSal96 <gabesal96@gmail.com>
2022-10-11 20:01:05 -0700

    Updated board generation method for the EnemyBoard object. Cells correctly populate the board at the specified positions.

--- Assets/Scripts/Objects/EnemyBoard.cs
+++ Assets/Scripts/Objects/EnemyBoard.cs
@@ -17,6 +17,26 @@ public class EnemyBoard : MonoBehaviour, IBoard
         _boardDimensionY = dimY;
 
         _cells = new GameObject[_boardDimensionX, _boardDimensionY];
+
+        PopulateBoard();
     }
 
+    private void PopulateBoard()
+    {
+        GameObject cellInstance = Resources.Load("Prefabs/Cell", typeof(GameObject)) as GameObject;
+
+        for (int i = 0; i < _boardDimensionX; i++)
+        {
+            for (int j = 0; j < _boardDimensionY; j++)
+            {
+                GameObject newCell = Instantiate(cellInstance, SetCellPosition(i, j), Quaternion.identity, this.transform);
+                _cells[i, j] = newCell;
+            }
+        }
+    }
+
+    private Vector3 SetCellPosition(int i, int j)
+    {
+        return new Vector3(-7.75f + (1.5f * i), 0f, 7.0f - (1.5f * j));
+    }
 }
